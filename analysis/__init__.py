fbbe99f477cf5efaf51e15380808fd0ae8321487
olegs <svytsop@gmail.com>
2025-11-11 22:24:41 +0200

    Add analisis and bussiness questions

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -1,9 +1,9 @@
 from .director_analysis import directors_increasing_ratings_trend, top_directors_by_high_rating_and_votes, director_career_span_analysis
-from .genre_analysis import genre_popularity_trend, genre_actor_cyclicality, genre_duration_rating_analysis
+from .genre_analysis import genre_popularity_trend, genre_actor_cyclicality, genre_duration_rating_analysis, genre_evolution_analysis, genre_combinations_analysis
 from .collaboration_analysis import writer_director_collaboration, actor_director_collaboration
 from .general import describe_dataframe
-from .tv_analysis import correlation_seasons_rating, top_episodes_by_votes_and_rating, genre_seasons_influence
-from .actor_analysis import actors_demography_stats
+from .tv_analysis import correlation_seasons_rating, top_episodes_by_votes_and_rating, genre_seasons_influence, avg_rating_long_series, season_rating_diff
+from .actor_analysis import actors_demography_stats, avg_rating_by_actor, young_actors_2000s
 
 __all__ = [
     "directors_increasing_ratings_trend",
@@ -12,11 +12,17 @@ __all__ = [
     "genre_popularity_trend",
     "genre_actor_cyclicality",
     "genre_duration_rating_analysis",
+    "genre_evolution_analysis",
+    "genre_combinations_analysis",
     "writer_director_collaboration",
     "actor_director_collaboration",
     "describe_dataframe",
     "correlation_seasons_rating",
     "top_episodes_by_votes_and_rating",
     "genre_seasons_influence",
-    "actors_demography_stats"
+    "avg_rating_long_series",
+    "season_rating_diff",
+    "actors_demography_stats",
+    "avg_rating_by_actor",
+    "young_actors_2000s"
 ]
